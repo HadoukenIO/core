38fd97a1044c1fe787bd6e3c51edc36f2e49e286
Harsimran Singh <HarsimranSingh@users.noreply.github.com>
2017-02-17 12:29:21 -0500

    Lint fix (#11)

--- src/browser/api_protocol/transport_strategy/ws_strategy.ts
+++ src/browser/api_protocol/transport_strategy/ws_strategy.ts
@@ -57,7 +57,7 @@ export class WebSocketStrategy extends ApiTransportBase<MessagePackage> {
         const identity = requestingConnection ? { uuid: requestingConnection.uuid, name: requestingConnection.uuid } : null;
 
         system.debugLog(1, `received external-adapter <= ${id} ${JSON.stringify(data)}`);
- 
+
         this.requestHandler.handle({
             data, ack, nack,
             identity: identity || id
