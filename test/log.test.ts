fcf7a83224270c5a8ac7da07d7a34dc29212d040
Ricardo De Peña <ricardo@isnull.co>
2017-02-28 09:45:58 -0500

    Feature/ts unit tests (#18)
    * added some ts unit tests
    
    * lic to files.

---
+++ test/log.test.ts
@@ -0,0 +1,72 @@
+/*
+Copyright 2017 OpenFin Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import * as assert from 'assert';
+import * as mockery from 'mockery';
+
+let lastVlogValue = '';
+let lastLogValue = '';
+const mockElectron = {
+    app: {
+        vlog: (level: string, val: string) => {
+            lastVlogValue = val;
+        },
+        log: (level: string, val: string) => {
+            lastLogValue = val;
+        }
+    }
+};
+
+mockery.registerMock('electron', mockElectron);
+mockery.enable();
+import * as log from '../src/browser/log';
+
+describe('log', () => {
+    describe('writeToLog', () => {
+
+        it('should stringify the object passed', () => {
+            const obj = { a: 'a', b: 1 };
+
+            log.writeToLog('info', obj);
+            assert(lastLogValue === JSON.stringify(obj));
+        });
+
+        it('should write strings', () => {
+            const str = 'test';
+
+            log.writeToLog('info', str);
+            assert(lastLogValue === str);
+        });
+
+        it('should stringify error objects', () => {
+            const err = new Error('Test');
+
+            log.writeToLog('info', err);
+            assert(JSON.parse(lastLogValue).message === err.message);
+        });
+
+        it('should use the vlog on debug', () => {
+            const str = 'test_vlog';
+
+            log.writeToLog('info', str, true);
+            assert(lastVlogValue === str);
+        });
+    });
+});
+
+after(() => {
+    mockery.deregisterAll();
+    mockery.disable();
+});
