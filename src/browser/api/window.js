54fa4a07590f8c449861bf2626234efa49dafc4b
Harsimran Singh <HarsimranSingh@users.noreply.github.com>
2017-04-12 16:13:31 -0400

    RUN-2907: use setIcon over setTaskbarIcon (#92)

--- src/browser/api/window.js
+++ src/browser/api/window.js
@@ -1967,7 +1967,7 @@ function setIcon(browserWindow, iconFilepath, errorCallback = () => {}) {
         if (icon.isEmpty()) {
             errorCallback();
         } else {
-            browserWindow.setTaskbarIcon(icon);
+            browserWindow.setIcon(icon);
         }
     }
 }
