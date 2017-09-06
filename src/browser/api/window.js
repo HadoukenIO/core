5dc4851c0a18e2a40d83f822aba1fce5e2c65e4c
Jonathan Eiten <jonathan@openfin.co>
2017-09-06 15:43:16 -0400

    RUN-3161 Wait for ready-to-show (#155)
    * refactor to use ready-to-show
    
    * move ready-to-show listener as high up as possible

--- src/browser/api/window.js
+++ src/browser/api/window.js
@@ -379,6 +379,11 @@ Window.create = function(id, opts) {
 
         browserWindow = BrowserWindow.fromId(id);
 
+        // set this listener up as soon as possible
+        browserWindow._whenReadyToShow = new Promise(ready => {
+            browserWindow.once('ready-to-show', ready);
+        });
+
         // this is a first pass at teardown. for now, push the unsubscribe
         // function for each subscription you make, on closed, remove them all
         // if you listen on 'closed' it will crash as your resources are
@@ -1745,14 +1750,11 @@ function applyAdditionalOptionsToWindowOnVisible(browserWindow, callback) {
     } else {
         browserWindow.once('visibility-changed', (event, isVisible) => {
             if (isVisible) {
-                if (browserWindow.isVisible()) {
+                if (browserWindow._readyToShowFiredAlready) {
                     callback();
-                    // Version 8: Will be visible on the next tick
-                    // TODO: Refactor to also use 'ready-to-show'
                 } else {
-                    setTimeout(() => {
-                        callback();
-                    }, 1);
+                    // Version 8: Will be visible on the next tick
+                    browserWindow._whenReadyToShow.then(callback);
                 }
             }
         });
