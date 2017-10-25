bb897e7c74ffce69ea40eef0e247719e7c1ceed3
whyn07m3 <whyn07m3@gmail.com>
2017-10-25 15:57:46 -0400

    RUN-3501 fixed plugins not working for consecutive apps with manifest

--- src/browser/api_protocol/api_handlers/window.js
+++ src/browser/api_protocol/api_handlers/window.js
@@ -146,7 +146,7 @@ function setWindowPreloadState(identity, message, ack) {
     const payload = message.payload;
     const windowIdentity = apiProtocolBase.getTargetWindowIdentity(identity);
 
-    Window.setWindowPluginState(windowIdentity, payload);
+    Window.setWindowPreloadState(windowIdentity, payload);
     ack();
 }
 
