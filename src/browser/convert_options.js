031da052a344f25fc58792f09aa38fa0d334677d
Steven Barbaro <StevenEBarbaro@gmail.com>
2017-12-28 13:47:10 -0500

    Update convert_options.js

--- src/browser/convert_options.js
+++ src/browser/convert_options.js
@@ -240,10 +240,6 @@ module.exports = {
             plugins: newOptions.plugins
         };
 
-        // v2Api Fallback for incorrect spelling
-        let ex = newOptions.expiremental || { v2Api: false };
-        newOptions.experimental.v2Api = ex.v2Api || newOptions.experimental.v2Api;
-
         newOptions.webPreferences.disableInitialReload = newOptions.experimental.disableInitialReload;
 
         if (coreState.argo['disable-web-security'] || newOptions.webSecurity === false) {
