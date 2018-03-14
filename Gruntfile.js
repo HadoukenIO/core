cc7197d95e161fe85b2490bf21f86d8efe917c69
Harsimran Singh <harsimran@openfin.co>
2018-03-14 11:34:24 -0400

    Update licenses for 2018

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 /*
-Copyright 2017 OpenFin Inc.
+Copyright 2018 OpenFin Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -61,22 +61,7 @@ Please contact OpenFin Inc. at sales@openfin.co to obtain a Commercial License.
 `;
 
 // Open-source license
-const openSourceLic = `/*
-Copyright 2017 OpenFin Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-`;
+const openSourceLic = `/*\n${ fs.readFileSync('./LICENSE') }*/\n`;
 
 module.exports = (grunt) => {
 
