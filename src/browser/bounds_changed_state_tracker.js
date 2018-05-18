5737e963ae2f8a554559f4c1ccb317ab3c223c3a
Thomas <tom@openfin.co>
2018-05-18 14:14:58 -0400

    emitting from inside bounds changing state tracker

--- src/browser/bounds_changed_state_tracker.js
+++ src/browser/bounds_changed_state_tracker.js
@@ -27,6 +27,8 @@ import * as Deferred from './deferred';
 let WindowGroups = require('./window_groups.js');
 import WindowGroupTransactionTracker from './window_group_transaction_tracker';
 import { toSafeInt } from '../common/safe_int';
+import ofEvents from './of_events';
+import route from '../common/route';
 
 const isWin32 = process.platform === 'win32';
 
@@ -440,6 +442,9 @@ function BoundsChangedStateTracker(uuid, name, browserWindow) {
     var _listeners = {
         'begin-user-bounds-change': () => {
             setUserBoundsChangeActive(true);
+            const cachedBounds = getCachedBounds();
+            const payload = { uuid, name, top: cachedBounds.y, left: cachedBounds.x };
+            ofEvents.emit(route.window('begin-user-bounds-changing', uuid, name), Object.assign(payload, cachedBounds));
         },
         'end-user-bounds-change': () => {
             setUserBoundsChangeActive(false);
