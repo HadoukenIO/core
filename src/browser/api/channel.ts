b6db516e62fb913d5d740b27a64bdc7b84e35e83
Thomas <tom@openfin.co>
2018-08-28 17:32:14 -0400

    hold

--- src/browser/api/channel.ts
+++ src/browser/api/channel.ts
@@ -5,7 +5,9 @@ import route from '../../common/route';
 import { RemoteAck, AckFunc, NackFunc, AckMessage, AckPayload, NackPayload } from '../api_protocol/transport_strategy/ack';
 import { sendToIdentity } from '../api_protocol/api_handlers/api_protocol_base';
 import { getExternalOrOfWindowIdentity } from '../core_state';
+import SubscriptionManager from '../subscription_manager';
 
+const subscriptionManager = new SubscriptionManager();
 const channelMap: Map<string, ProviderIdentity> = new Map();
 const pendingChannelConnections: Map<string, any[]> = new Map();
 
@@ -23,7 +25,8 @@ interface AckToSender {
     };
 }
 
-const getChannelId = (uuid: string, name: string, channelName: string): string => {
+const getChannelId = (identity: Identity, channelName: string): string => {
+    const { uuid, name } = identity;
     return `${uuid}/${name}/${channelName}`;
 };
 
@@ -39,20 +42,31 @@ const createAckToSender = (identity: Identity, messageId: number, providerIdenti
     };
 };
 
-const createChannelTeardown = (providerIdentity: ProviderIdentity): void => {
-    // When channel exits, remove from channelMap
-    const { uuid, name, isExternal, channelId } = providerIdentity;
-    const closedEvent = isExternal ? route.externalApplication('closed', uuid) : route.window('closed', uuid, name);
-    ofEvents.once(closedEvent, () => {
+const constructOnDisconnection = (providerIdentity: ProviderIdentity): () => void => {
+    return () => {
+        const { channelId } = providerIdentity;
+
         channelMap.delete(channelId);
         ofEvents.emit(route.channel('disconnected'), providerIdentity);
         // Need channel-disconnected for compatibility with 9.61.34.*
         ofEvents.emit(route.channel('channel-disconnected'), providerIdentity);
-    });
-
-    // For some reason this is captured sometimes when window closed is not
-    ofEvents.once(route.application('closed', uuid), () => channelMap.delete(channelId));
+    };
 };
+
+// const createChannelTeardown = (providerIdentity: ProviderIdentity): void => {
+//     // When channel exits, remove from channelMap
+//     const { uuid, name, isExternal, channelId } = providerIdentity;
+//     const closedEvent = isExternal ? route.externalApplication('closed', uuid) : route.window('closed', uuid, name);
+//     ofEvents.once(closedEvent, () => {
+//         channelMap.delete(channelId);
+//         ofEvents.emit(route.channel('disconnected'), providerIdentity);
+//         // Need channel-disconnected for compatibility with 9.61.34.*
+//         ofEvents.emit(route.channel('channel-disconnected'), providerIdentity);
+//     });
+
+//     // For some reason this is captured sometimes when window closed is not
+//     ofEvents.once(route.application('closed', uuid), () => channelMap.delete(channelId));
+// };
 
 export module Channel {
     export function addEventListener(targetIdentity: Identity, type: string, listener: (eventPayload: EventPayload) => void) : () => void {
@@ -65,31 +79,12 @@ export module Channel {
         };
     }
 
-    export function getChannelByChannelId(channelId: string): ProviderIdentity {
-        return channelMap.get(channelId);
-    }
-
     export function getAllChannels(): ProviderIdentity[] {
         const allChannels: ProviderIdentity[] = [];
         channelMap.forEach(channel => {
             allChannels.push(channel);
         });
         return allChannels;
-    }
-
-    export function getChannelByIdentity(identity: Identity): ProviderIdentity[] {
-        const { uuid, name } = identity;
-        const providerIdentity: ProviderIdentity[] = [];
-        channelMap.forEach(channel => {
-            if (channel.uuid === uuid) {
-                if (!name) {
-                    providerIdentity.push(channel);
-                } else if (channel.name === name) {
-                    providerIdentity.push(channel);
-                }
-            }
-        });
-        return providerIdentity;
     }
 
     export function getChannelByChannelName(channelName: string, channelArray?: ProviderIdentity[]): ProviderIdentity|undefined {
@@ -104,21 +99,20 @@ export module Channel {
     }
 
     export function createChannel(identity: Identity, channelName: string, allChannels: ProviderIdentity[]): ProviderIdentity {
-        const { uuid, name } = identity;
-        const providerApp = getExternalOrOfWindowIdentity(identity);
-
         // If a channel has already been created with that channelName
         if (Channel.getChannelByChannelName(channelName, allChannels)) {
             const nackString = 'Channel creation failed: Please note that only one channel may be registered per channelName.';
             throw new Error(nackString);
         }
 
-        const channelId = getChannelId(uuid, name, channelName);
-
+        const providerApp = getExternalOrOfWindowIdentity(identity);
+        const channelId = getChannelId(identity, channelName);
         const providerIdentity = { ...providerApp, channelName, channelId };
         channelMap.set(channelId, providerIdentity);
 
-        createChannelTeardown(providerIdentity);
+        subscriptionManager.registerSubscription(constructOnDisconnection(providerIdentity), identity, channelId);
+
+        // createChannelTeardown(providerIdentity);
         // Used internally by adapters for pending connections and onChannelConnect
         ofEvents.emit(route.channel('connected'), providerIdentity);
         // Need channel-connected for compatibility with 9.61.34.*
