fcb6ae6d5fcd763cb1e1e160493ee9a61c3d3e3c
whyn07m3 <whyn07m3@gmail.com>
2018-09-25 10:55:21 -0400

    RUN-4641 support loading local dev js-adapter

--- src/browser/runtime_p2p/peer_connection_manager.ts
+++ src/browser/runtime_p2p/peer_connection_manager.ts
@@ -1,7 +1,21 @@
 import { EmitterMap } from './emitter_map';
 import { createHash } from 'crypto';
-import { connect, Identity, Fin } from 'hadouken-js-adapter';
+import { Identity, Fin } from 'hadouken-js-adapter';
 import { EventEmitter } from 'events';
+import * as path from 'path';
+
+/*
+    Try loading local dev js-adapter, if failed, resort to
+    loading production js-adapter, which is included as a dependency
+ */
+let connect: any;
+try {
+    const jsAdapterDevPath = path.resolve(process.cwd(), 'resources', 'js-adapter', 'src', 'main.js');
+    // tslint:disable-next-line
+    connect = require(jsAdapterDevPath).connect; // DEV js-adapter
+} catch (error) {
+    connect = require('hadouken-js-adapter').connect; // PROD js-adapter
+}
 
 //TODO: This belongs somewhere else, need to find out where.
 export interface Identity {
