f7264da4e8bdedb645ccaff0fac2e4632fddcdc5
Ricardo De Peña <ricardo@isnull.co>
2018-11-07 13:13:21 -0500

    Feature/run 4106 multi runtime groups (#615)
    * Join group works on my machine
    
    * Leave group now working
    
    * Closing a window now raises the leave group event
    
    * Proxy close no longer closes real window
    
    * Removing proxy windows on force closed runtimes
    
    * back to groupUuid
    
    * Fixed Merge group event bug
    
    * Cross runtime window group events propagating.
    
    * classing it up
    
    * Make sure async functions resolve to the API
    
    * disbanding now clears a group only comprised of proxy windows
    
    * Minimum version of the runtime set to .37 for cross runtime grouping.
    
    * Handle merge groups as well.
    
    * Fixed issue where sourceIdentity was undefined
    
    * _options now available for proxy windows.
    
    * put back grunt tasks.

--- src/browser/api/window.js
+++ src/browser/api/window.js
@@ -654,12 +654,13 @@ Window.create = function(id, opts) {
             var _win = coreState.getWindowByUuidName(uuid, name) || {};
             var _groupUuid = _win.groupUuid || null;
 
-            if (event.groupUuid === _groupUuid) {
+            //if the groupUuid's match or the _win object has no uuid (the window has closed)
+            if (event.groupUuid === _groupUuid || _win.uuid === void 0) {
                 var payload = event.payload;
 
                 payload.name = name;
                 /* jshint ignore:start */
-                payload.uuid = _win.app_uuid;
+                payload.uuid = _win.app_uuid || event.uuid;
                 /* jshint ignore:end */
 
                 if (payload.reason === 'disband') {
@@ -1401,16 +1402,7 @@ Window.isShowing = function(identity) {
 
 
 Window.joinGroup = function(identity, grouping) {
-    let identityOfWindow = Window.wrap(identity.uuid, identity.name);
-    let groupingOfWindow = Window.wrap(grouping.uuid, grouping.name);
-    let identityBrowserWindow = identityOfWindow && identityOfWindow.browserWindow;
-    let groupingBrowserWindow = groupingOfWindow && groupingOfWindow.browserWindow;
-
-    if (!identityBrowserWindow || !groupingBrowserWindow) {
-        return;
-    }
-
-    WindowGroups.joinGroup(identityOfWindow, groupingOfWindow);
+    return WindowGroups.joinGroup({ uuid: identity.uuid, name: identity.name }, { uuid: grouping.uuid, name: grouping.name });
 };
 
 
@@ -1422,7 +1414,7 @@ Window.leaveGroup = function(identity) {
     }
 
     let openfinWindow = Window.wrap(identity.uuid, identity.name);
-    WindowGroups.leaveGroup(openfinWindow);
+    return WindowGroups.leaveGroup(openfinWindow);
 };
 
 
@@ -1436,16 +1428,7 @@ Window.maximize = function(identity) {
 
 
 Window.mergeGroups = function(identity, grouping) {
-    let identityOfWindow = Window.wrap(identity.uuid, identity.name);
-    let groupingOfWindow = Window.wrap(grouping.uuid, grouping.name);
-    let identityBrowserWindow = identityOfWindow && identityOfWindow.browserWindow;
-    let groupingBrowserWindow = groupingOfWindow && groupingOfWindow.browserWindow;
-
-    if (!identityBrowserWindow || !groupingBrowserWindow) {
-        return;
-    }
-
-    WindowGroups.mergeGroups(identityOfWindow, groupingOfWindow);
+    return WindowGroups.mergeGroups({ uuid: identity.uuid, name: identity.name }, { uuid: grouping.uuid, name: grouping.name });
 };
 
 
@@ -1954,7 +1937,7 @@ function createWindowTearDown(identity, id, browserWindow, _boundsChangedHandler
         coreState.removeChildById(id);
 
         // remove window from any groups it belongs to
-        WindowGroups.leaveGroup(ofWindow);
+        promises.push(WindowGroups.leaveGroup(ofWindow));
 
         promises.push(handleSaveStateAlwaysResolve());
 
