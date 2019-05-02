628cb0da620b2ef8b77e5aa2b39d31a13b90536a
tomer-openfin <47791895+tomer-openfin@users.noreply.github.com>
2019-05-02 11:53:04 -0400

    Update window.js

--- src/browser/api/window.js
+++ src/browser/api/window.js
@@ -92,7 +92,7 @@ let browserWindowEventMap = {
     },
     'opacity-changed': {
         decorator: opacityChangedDecorator
-    }, 
+    },
     'user-movement-disabled': {
         topic: 'frame-disabled'
     },
