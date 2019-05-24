1d6b7554358879ad1b08f7c120450812eb002713
whyn07m3 <whyn07m3@gmail.com>
2019-05-24 11:10:43 -0400

    RUN-5265 cleaned up external window info object

--- src/shapes.ts
+++ src/shapes.ts
@@ -1,6 +1,10 @@
 
 import { PortInfo } from './browser/port_discovery';
-import { BrowserWindow as BrowserWindowElectron, NativeWindowInfo as NativeWindowInfoElectron } from 'electron';
+import {
+    BrowserWindow as BrowserWindowElectron,
+    NativeWindowInfo as NativeWindowInfoElectron,
+    Process as ProcessElectron
+} from 'electron';
 import { ERROR_BOX_TYPES } from './common/errors';
 import { AnchorType } from '../js-adapter/src/shapes';
 
@@ -427,12 +431,6 @@ export interface CoordinatesXY {
     y: number;
 }
 
-export interface ProcessInfo {
-    imageName: string;
-    injected: boolean;
-    pid: number;
-}
-
 // This mock is for window grouping accepting external windows
 interface BrowserWindowMock extends BrowserWindowElectron {
     _options: WindowOptions;
@@ -451,10 +449,18 @@ export interface ExternalWindow extends BrowserWindowElectron {
     uuid: string;
 }
 
-export interface NativeWindowInfo extends NativeWindowInfoElectron {
+export interface Process extends Omit<ProcessElectron, 'imageName'> {
+    injected: boolean;
+    pid: number;
+}
+
+export interface NativeWindowInfo extends Omit<NativeWindowInfoElectron, 'process'> {
+    process: Process;
     name: string;
     uuid: string;
 }
+
+export type NativeWindowInfoLite = Pick<NativeWindowInfo, 'name'|'process'|'title'|'uuid'|'visible'>;
 
 export type GroupWindow = (ExternalWindow | OpenFinWindow) & {
     isExternalWindow?: boolean;
