ef6723de006132754e79f40388ad171299b63c4e
Michael Coates <MichaelMCoates@gmail.com>
2019-06-28 11:05:19 -0400

    Change externalNameAlias to nameAlias

--- src/browser/api/channel.ts
+++ src/browser/api/channel.ts
@@ -138,8 +138,8 @@ export module Channel {
         const connectingWindow = getExternalOrOfWindowIdentity(identity);
         const providerIdentity = Channel.getChannelByChannelName(channelName);
 
-        if (connectingWindow && connectingWindow.isExternal && connectionPayload.externalNameAlias) {
-            identity.name = connectionPayload.externalNameAlias;
+        if (connectingWindow && connectingWindow.isExternal && connectionPayload && connectionPayload.nameAlias) {
+            identity.name = connectionPayload.nameAlias;
         }
 
         if (providerIdentity) {
