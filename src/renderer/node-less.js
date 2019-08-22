9fad113a70dbaa5b1dc7fc73b4609569fe12a5b7
Pierre Baize <pierre@openfin.co>
2019-08-22 15:36:22 -0400

    conditional

--- src/renderer/node-less.js
+++ src/renderer/node-less.js
@@ -135,14 +135,17 @@ const registerAPI = (w, routingId, isMainFrame, isSameOriginIframe, isCrossOrigi
         console.error(error.stack);
         electron.ipcRenderer.send(routingId, 'api-injection-failed', routingId);
     } finally {
-        delete w.require;
-        delete w.process;
-        delete w.module;
-        delete w.Buffer;
-        delete w.routingId;
-        delete w.isMainFrame;
-        delete w.global;
-        delete w.getFrameData;
+        if (w) {
+            delete w.require;
+            delete w.process;
+            delete w.module;
+            delete w.Buffer;
+            delete w.routingId;
+            delete w.isMainFrame;
+            delete w.global;
+            delete w.getFrameData;
+            w = undefined;
+        }
     }
 
     susbcribeForTeardown(routingId, teardownHandlers);
