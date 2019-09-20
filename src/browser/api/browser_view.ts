f939c5218d50f171e9b1ffe878dae7e7afdb9a71
Pierre Baize <pierre@openfin.co>
2019-09-20 16:07:35 -0400

    re-delete

--- src/browser/api/browser_view.ts
+++ src/browser/api/browser_view.ts
@@ -42,7 +42,6 @@ export async function create(options: BrowserViewOpts) {
     const ofView = addBrowserView(fullOptions, view);
     hookWebContentsEvents(view.webContents, options, 'view', route.view);
     await attach(ofView, options.target);
-    view.webContents.loadURL(options.url || 'about:blank');
     of_events.emit(route.view('created', ofView.uuid, ofView.name), {
         name: ofView.name,
         uuid: ofView.uuid,
