070d0a88ba5491da3186d00d03313d6228352b90
Michael M. Coates <MichaelMCoates@gmail.com>
2019-10-16 10:00:09 -0400

    Remove getEntityIdentity from nameAlias check (#989)

--- src/browser/api/channel.ts
+++ src/browser/api/channel.ts
@@ -128,11 +128,9 @@ export module Channel {
 
     export function connectToChannel(identity: Identity, payload: any, messageId: number, ack: AckFunc, nack: NackFunc): void {
         const { channelName, payload: connectionPayload } = payload;
-
-        const connectingWindow = getEntityIdentity(identity);
         const providerIdentity = Channel.getChannelByChannelName(channelName);
 
-        if (connectingWindow && connectingWindow.isExternal && connectionPayload && connectionPayload.nameAlias) {
+        if (connectionPayload && connectionPayload.nameAlias) {
             identity.name = connectionPayload.nameAlias;
         }
 
@@ -177,9 +175,7 @@ export module Channel {
         const { uuid, name, payload: messagePayload, action: channelAction, providerIdentity } = payload;
         const intendedTargetIdentity = { uuid, name };
 
-        const messagingWindow = getEntityIdentity(identity);
-
-        if (messagingWindow && messagingWindow.isExternal && messagePayload && messagePayload.nameAlias) {
+        if (messagePayload && messagePayload.nameAlias) {
             identity.name = messagePayload.nameAlias;
         }
 
